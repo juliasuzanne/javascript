3c5661aca68c9302dc0f76deb815449a07fcc3b9
Julia Grimes <julia.s.grimes@gmail.com>
2023-12-18 13:14:57 -0500

    Solve hackerrank easy problem

--- cat-and-mouse.js
+++ cat-and-mouse.js
@@ -1,1 +1,15 @@
-function catAndMouse(x, y, z) {}
+function catAndMouse(x, y, z) {
+  let catA = 0;
+  let catB = 0;
+  catA = Math.abs(x - z);
+  catB = Math.abs(y - z);
+  if (catA < catB) {
+    return "Cat A";
+  } else if (catA > catB) {
+    return "Cat B";
+  } else {
+    return "Mouse C";
+  }
+}
+
+console.log(catAndMouse(2, 6, 5));
