83841ecce9bf17db50bf8280b967a58dafbe6b3a
Julia Grimes <julia.s.grimes@gmail.com>
2023-12-20 16:24:20 -0500

    check each item by index after sort, check remainder

--- divisible-sum-pairs.js
+++ divisible-sum-pairs.js
@@ -1,3 +1,16 @@
 function divisibleSumPairs(n, k, ar) {
   // Write your code here
+  ar = ar.sort();
+  let count = 0;
+
+  for (let j = 0; j < ar.length; j++) {
+    for (let i = j + 1; i < ar.length; i++) {
+      if ((ar[i] + ar[j]) % k === 0) {
+        count++;
+      }
+    }
+  }
+  return count;
 }
+
+console.log(divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2]));
