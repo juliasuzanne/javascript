c4164bfc185e15dff7349734e1e21d5c4709ba42
Julia Grimes <julia.s.grimes@gmail.com>
2023-12-25 17:43:05 -0500

    Hackerrank challenge

---
+++ climbing-the-leaderboard.js
@@ -0,0 +1,5 @@
+function climbingLeaderboard(ranked, player) {
+  for (let i = 0; i < player.length; i++) {}
+}
+
+console.log(climbingLeaderboard([100, 90, 90, 80, 75, 60], [50, 65, 77, 90, 102]));
