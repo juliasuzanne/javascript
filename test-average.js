6646864c03aa65273765760eee28fafaccaaac6d
Julia Grimes <julia.s.grimes@gmail.com>
2023-12-25 21:34:31 -0500

    Function to find place in array

--- test-average.js
+++ test-average.js
@@ -7,24 +7,29 @@ function findNumberPlaceInArray(array, number) {
 }
 
 function recursiveFunction(array, number, low, high, index) {
-  if (array[index] > number) {
-    console.log("BIG");
-    high = index;
-    index = getAverage(low, high);
-    console.log("LOW: " + low + " HIGH: " + high + " INDEX: " + index);
-  } else if (array[index] < number) {
-    console.log("SMALL");
-    low = index;
-    console.log(low);
-    index = getAverage(low, high);
-    console.log(" TS LOW: " + low + " HIGH: " + high + " INDEX: " + index);
+  console.log("LOW: " + low + " HIGH: " + high + " INDEX: " + index);
+
+  if (array[index] < number && array[index - 1] > number) {
+    console.log("FOUND 1");
+    console.log(index + 1);
+  } else if (array[index] > number && array[index + 1] < number) {
+    console.log("FOUND 2");
+    console.log(index + 2);
   } else {
-    console.log("else");
-    return 100;
+    if (number < array[index]) {
+      low = index;
+      index = getAverage(low, high);
+      recursiveFunction(array, number, low, high, index);
+    } else if (number > array[index]) {
+      high = index;
+      index = getAverage(low, high);
+      recursiveFunction(array, number, low, high, index);
+    }
   }
 }
 
 function getAverage(low, high) {
   return Math.floor((high - low) / 2) + low;
 }
-console.log(findNumberPlaceInArray([3, 5, 8, 100, 400], 2));
+
+findNumberPlaceInArray([100, 50, 20, 10, 5, 2], 90);
